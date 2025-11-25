4501b88ecea989c72399c1c020e4d05b5dda86e8
hybridmlmplan <osfdoi@gmail.com>
2025-11-25 22:28:42 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend running successfully!");
+});
+
+// Server start
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
