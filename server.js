cdb26fbcc624d336687af28eb058cd67b46ed39d
hybridmlmplan <osfdoi@gmail.com>
2025-11-27 19:21:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
+const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./routes/auth");
-import express from "express";
-import cors from "cors";
-const app = express();
-app.use(cors());
-app.use("/api/auth", authRoutes);
 
+const app = express();
+
+// Middlewares
+app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
