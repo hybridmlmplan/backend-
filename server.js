7aab33b470db3eb1ec794988672ed3f02510230f
hybridmlmplan <osfdoi@gmail.com>
2025-11-27 19:42:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,29 +7,9 @@ const app = express();
 
 // MongoDB connect
 mongoose
-  .connect("YOUR_MO
-// Middlewares
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use("/api/auth", authRoutes);
-
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend running successfully!");
-});
-
-// Server start
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
-
-const express = require("express");
-const cors = require("cors");
-const authRoutes = require("./routes/auth");
-
-const app = express();
+  .connect("YOUR_MONGO_CONNECTION_STRING_HERE")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
 
 // Middlewares
 app.use(cors());
@@ -47,4 +27,3 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
-o
