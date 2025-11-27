33e3349ecc005bff438fa8e37e549f4e487ec8dc
hybridmlmplan <osfdoi@gmail.com>
2025-11-27 15:34:08 +0530

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// LOGIN API
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check empty fields
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email & Password required" });
+    }
+
+    // Find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Check password match
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Password" });
+    }
+
+    // Create login token
+    const token = jwt.sign({ id: user._id }, "SECRETKEY", {
+      expiresIn: "7d",
+    });
+
+    res.json({
+      message: "Login Success",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
