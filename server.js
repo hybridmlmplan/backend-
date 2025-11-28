189fe81ff490a9989e6701b5397db34923320951
hybridmlmplan <osfdoi@gmail.com>
2025-11-28 14:50:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,24 @@ const authRoutes = require("./routes/auth");
 
 const app = express();
 
-// MongoDB connect
-mongoose
-  .connect("YOUR_MONGO_CONNECTION_STRING_HERE")
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.log("MongoDB Error:", err));
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connect
+mongoose
+  .connect(process.env.MONGO_URL || "YOUR_MONGO_STRING")
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log("MongoDB Error:", err));
+
+// Test
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("Backend running successfully!");
-});
-
-// Server start
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
