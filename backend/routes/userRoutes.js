3d47cdb45571646420a5faf4159e007dfbec269c
hybridmlmplan <osfdoi@gmail.com>
2025-11-28 17:46:22 +0530

    Delete backend/routes/userRoutes.js

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { testUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/test", testUser);
-
-export default router;
