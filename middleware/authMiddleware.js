077c756c5c61ab89e62ed309ea3e4b282c3e9879
hybridmlmplan <osfdoi@gmail.com>
2025-12-04 16:20:24 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
-export const authMiddleware = async (req, res, next) => {
+export default async function authMiddleware(req, res, next) {
   try {
     const token = req.headers["authorization"];
 
