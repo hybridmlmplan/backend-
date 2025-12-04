22cd6016dfcd0285ccd8b9649610fde848d0641c
hybridmlmplan <osfdoi@gmail.com>
2025-12-04 13:28:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 
 import authRoute from './routes/auth.js';
+import incomeRoute from './routes/income.js';
 
 dotenv.config();
 connectDB();
@@ -13,5 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/auth', authRoute);
+app.use('/api/income', incomeRoute);
 
-app.listen(5000, () => console.log('Backend running on PORT 5000'));
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Backend running on PORT ${process.env.PORT || 5000}`)
+);
