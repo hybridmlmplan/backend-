6a120aa028026c20454748f0fb98057250ffc5ce
hybridmlmplan <osfdoi@gmail.com>
2025-12-06 13:51:31 +0530

    Update genealogy.js

--- routes/genealogy.js
+++ routes/genealogy.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   getTreeByUser,
   placeUserInTree,
-  getDownline
+  getDownline,
+  getDirectsByUser
 } from "../controllers/genealogyController.js";
 
 import {
@@ -12,13 +13,24 @@ import {
 
 const router = express.Router();
 
+// =============================
 // GET TREE
+// =============================
 router.get("/tree/:userId", protect, getTreeByUser);
 
+// =============================
 // PLACE USER (admin)
+// =============================
 router.post("/place-user", verifyAdmin, placeUserInTree);
 
+// =============================
 // GET DOWNLINE (optional level)
+// =============================
 router.get("/downline/:userId/:level?", protect, getDownline);
+
+// ====================================
+// NEW: GET DIRECT TEAM LIST
+// ====================================
+router.get("/directs/:userId", protect, getDirectsByUser);
 
 export default router;
