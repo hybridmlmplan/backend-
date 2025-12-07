ffe95bd368e8831bbb6c6570b82437e581071f56
hybridmlmplan <osfdoi@gmail.com>
2025-12-07 16:05:46 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,16 @@
+// user doc additions (high-level)
+{
+  packageCode,        // Sp/Gp/Rp
+  rank,               // e.g., 'Sp Star', 'Rp Diamond Star'
+  levelCounts: {      // counts for level eligibility
+    level1Directs: Number,
+    level2Members: Number,
+    level3Members: Number,
+    // ...
+  },
+  isRubyOrAbove: Boolean,
+  isDiamondOrAbove: Boolean,
+  travelFundEligible: Boolean,
+  carFundEligible: Boolean,
+  houseFundEligible: Boolean
+}
