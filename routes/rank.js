6a76839e47caaef7ea6df3b042478c4eb3effc6e
hybridmlmplan <osfdoi@gmail.com>
2025-12-07 05:55:57 +0530

    Delete routes directory

--- routes/rank.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// TEST API
-router.get("/", (req, res) => {
-  res.json({ message: "Rank API working" });
-});
-
-export default router;
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// TEST API
-router.get("/", (req, res) => {
-  res.json({ message: "User API working" });
-});
-
-export default router;
